f3bb900ef5ec11d065b1b7ae48af9d3a887d9dce
Твое Имя <seterkhanuly@gmail.com>
2026-04-17 21:51:51 +0500

    Добавлен вывод страны, температуры и скорости ветра

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ data = response.json()
 
 latitude = data["results"][0]["latitude"]
 longitude = data["results"][0]["longitude"]
+country = data["results"][0]["country"]
 
-print("Город:", city)
+weather_url = "https://api.open-meteo.com/v1/forecast"
+weather_params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "current_weather": True
+}
+
+weather_response = requests.get(weather_url, params=weather_params)
+weather_data = weather_response.json()
+
+print("Страна:", country)
 print("Широта:", latitude)
 print("Долгота:", longitude)
+print("Температура:", weather_data["current_weather"]["temperature"], "°C")
+print("Скорость ветра:", weather_data["current_weather"]["windspeed"], "км/ч")
