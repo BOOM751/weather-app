164b70f5b27de31a4a60a731217306d8e732de23
Твое Имя <seterkhanuly@gmail.com>
2026-04-17 22:05:48 +0500

    Добавлена обработка ошибок и вывод найденного названия города

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ params = {
 response = requests.get(url, params=params)
 data = response.json()
 
-latitude = data["results"][0]["latitude"]
-longitude = data["results"][0]["longitude"]
-country = data["results"][0]["country"]
+if "results" in data:
+    latitude = data["results"][0]["latitude"]
+    longitude = data["results"][0]["longitude"]
+    country = data["results"][0]["country"]
+    found_city = data["results"][0]["name"]
 
-weather_url = "https://api.open-meteo.com/v1/forecast"
-weather_params = {
-    "latitude": latitude,
-    "longitude": longitude,
-    "current_weather": True
-}
+    weather_url = "https://api.open-meteo.com/v1/forecast"
+    weather_params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current_weather": True
+    }
 
-weather_response = requests.get(weather_url, params=weather_params)
-weather_data = weather_response.json()
+    weather_response = requests.get(weather_url, params=weather_params)
+    weather_data = weather_response.json()
 
-print("Страна:", country)
-print("Широта:", latitude)
-print("Долгота:", longitude)
-print("Температура:", weather_data["current_weather"]["temperature"], "°C")
-print("Скорость ветра:", weather_data["current_weather"]["windspeed"], "км/ч")
+    print("Город:", found_city)
+    print("Страна:", country)
+    print("Широта:", latitude)
+    print("Долгота:", longitude)
+    print("Температура:", weather_data["current_weather"]["temperature"], "°C")
+    print("Скорость ветра:", weather_data["current_weather"]["windspeed"], "км/ч")
+else:
+    print("Город не найден")
